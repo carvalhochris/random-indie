45c6692795645e1be1b04748731da9dc7a97f0ae
Christopher <kriss17388+dev@gmail.com>
2023-01-10 20:08:41 +0000

    -pre origin

---
+++ src/PlayTrack.js
@@ -0,0 +1,70 @@
+import React from 'react';
+// import Howler from './Howler'
+
+class PlayTrack extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      loadedCharacter: false,
+      name: null,
+      file: null,
+      artist: null,
+    }
+  }
+
+  getSong() {
+    const randomNumber = Math.round( Math.random() * 2 + 1)
+    console.log(randomNumber)
+    const url = `http://localhost:8000/songs/${randomNumber}/`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          name: data.name,
+          file: data.file,
+          artist: data.artist,
+          loadedCharacter: true,
+        })
+      })
+  }
+
+  // pauseSong = () => {
+  //   this.state.file.pause();
+  // }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.loadedCharacter && 
+            <div>
+              <h2>{this.state.name}</h2>
+              <h3>by {this.state.artist}</h3>
+              {/* <button onclick={this.pauseSong} type="button">Pause Audio</button> */}
+              <audio id="audio1" src={this.state.file} preload="auto" autoPlay controls controlsList="nodownload noplaybackrate"/>
+                {/* <button onclick={document.getElementById('audio1').play()}>Play</button> 
+                <button onclick={document.getElementById('audio1').pause()}>Pause</button>  */}
+                {/* <button onclick="document.getElementById('audio1').volume += 0.1">Vol +</button> 
+                <button onclick="document.getElementById('audio1').volume -= 0.1">Vol -</button>  */}
+              {/* <button onclick="playAudio()" type="button">Play Audio</button>
+              <button onclick="pauseAudio()" type="button">Pause Audio</button>  */}
+            {/* <button onClick={sound.play()}>HELLO</button> */}
+            </div>
+        }
+        <br></br>
+        <button 
+          type="button" 
+          onClick={() => this.getSong()} 
+          className="btn"
+        >
+          Click to discover
+        </button>
+        <br></br>
+        {/* <a onClick="togglePlay()">toggle play</a> */}
+        
+      </div>
+    )
+  }
+}
+
+export default PlayTrack;
---
+++ src/UYS.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class UYS extends React.Component {
+
+    render (){
+      return (
+        <div>
+            <br></br>
+            <p>Submit <a 
+              href="https://unlockyoursound.com/submit/" 
+              target="_blank" 
+              rel="noreferrer">Music</a>
+            </p>
+            <p 
+              className="uys">powered by <a 
+              href="https://unlockyoursound.com/" 
+              target="_blank" 
+              rel="noreferrer">Unlock Your Sound</a>
+              </p>
+        </div>
+      )
+    }
+}
+export default UYS;
